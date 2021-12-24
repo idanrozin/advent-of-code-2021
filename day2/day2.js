3694ff24de8221d21d4ed542960f9204c35a05c9
idanrozin <idanrozin@gmail.com>
2021-12-24 20:56:56 +0200

    finished function of day 2 question 2

--- day2/day2.js
+++ day2/day2.js
@@ -4,9 +4,9 @@ const ReadFilesUtils = require('../utils/utils.js');
 
 const inputs = new ReadFilesUtils(__dirname).inputAsStrings;
 
+const UP_DOWN_REGEX = new RegExp("(up|down)\\s*(\\d+)");
+const FORWARD_REGEX = new RegExp("forward\\s*(\\d+)");
 const calcHorizontalPosAndDepth = (inputs) => {
-    const UP_DOWN_REGEX = new RegExp("(up|down)\\s*(\\d+)");
-    const FORWARD_REGEX = new RegExp("forward\\s*(\\d+)");
     const findForwards = str => str.includes('forward');
     
     const justForwards = inputs.filter(findForwards);
@@ -29,7 +29,42 @@ const calcHorizontalPosAndDepth = (inputs) => {
         forwardSum, 
         upDownSum 
     };
-}
+};
+
+const calcHorizontalPosAndDepthWithAim = (inputs) => {
+    let aim = 0, forwardPos = 0 , depthPos = 0;
+    
+    inputs.forEach(el => {
+        const forward = el.match(FORWARD_REGEX);
+        if (forward) {
+
+            let [ , value ] = forward;
+            value = Number(value);
+            depthPos += value * aim;
+            forwardPos += value;
+
+        } else {
+        
+            const upDown = el.match(UP_DOWN_REGEX);
+            let [ , upOrDown, value ] = upDown;
+            value = Number(value);
+        
+            const operate = upOrDown === "up" ? -Math.abs(value) : value;
+            aim += operate;
+
+        }   
+    });
+
+   
+    return {
+        forwardPos,
+        depthPos
+    };
+
+};
 
 const { forwardSum, upDownSum } = calcHorizontalPosAndDepth(inputs);
-console.log(`Product of up-down and forward ${upDownSum * forwardSum}`);
+console.log(`Product of horizontal position and depth: ${upDownSum * forwardSum}`);
+
+const { forwardPos, depthPos } = calcHorizontalPosAndDepthWithAim(inputs);
+console.log(`Product of horizontal position and depth with aim: ${forwardPos * depthPos}`);
