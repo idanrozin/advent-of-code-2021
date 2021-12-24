bf3a2266ff4767c0bf6a9876bccf4a41523580a2
idanrozin <idanrozin@gmail.com>
2021-12-24 16:19:54 +0200

    finished question 2

--- day1/day1.js
+++ day1/day1.js
@@ -2,11 +2,11 @@ const ReadFilesUtils = require('../utils/utils.js');
 
 const nums = new ReadFilesUtils(__dirname).inputAsNumbers;
 
-const countIncreases = () => {
+const countIncreases = (numbers) => {
     let count = 0;
 
-    for (let i = 0; i < nums.length - 1; i++) {
-        if (nums[i] < nums[i + 1]) {
+    for (let i = 0; i < numbers.length - 1; i++) {
+        if (numbers[i] < numbers[i + 1]) {
              count++;
         }
     }
@@ -14,4 +14,19 @@ const countIncreases = () => {
     return count;
 }
 
-console.log('increases', countIncreases());
+
+// get all sums of three consecutive elements
+const sumThrees = (numbers) => {
+    const threesSumArray = [];
+
+    for (let i = 0; i < numbers.length - 2; i++) {
+        const subArray = numbers.slice(i, i + 3);
+        threesSumArray.push(subArray.reduce((acc, curr) => acc + curr, 0));
+    }
+
+    return threesSumArray;
+}
+
+
+console.log('increases', countIncreases(nums));
+console.log('threes increases', countIncreases(sumThrees(nums)));
