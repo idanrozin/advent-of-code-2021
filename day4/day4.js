e47ac3c11d27334392125d9dbc335c5e917cf40f
idanrozin@gmail.com <idanrozin@gmail.com>
2022-01-03 22:59:49 +0200

    finished day4 question 2

--- day4/day4.js
+++ day4/day4.js
@@ -18,18 +18,26 @@ const arrangeDataAsBoardsMatrix = (boards) => {
     });
 }
 
+const isWin = (board, line, colIndex) => {
+	return line.every(n => n.marked) || board.every(ln => ln.length === 0 || ln[colIndex].marked);
+};
+
+const sumAllUnmarked = (board) => board.map(b => b.reduce((acc,curr) => curr.marked ? acc : acc + curr.number , 0)).reduce((acc, curr) => acc + curr, 0);
+
+const getBoardsAndNumbersList = (inputs) => {
+	let [numbers, ...boards] = inputs.split(/\n\s*\n/);
+	numbers = numbers.split(",").map(Number);
+	const allBoards = arrangeDataAsBoardsMatrix(boards);
+	return [
+		numbers,
+		allBoards
+	];
+}
+
 const getBingoScore = (inputs) => {
 
-	const isWin = (board, line, colIndex) => {
-		return line.every(n => n.marked) || board.every(ln => ln[colIndex].marked);
-	};
-
-	const sumAllUnmarked = (board) => board.map(b => b.reduce((acc,curr) => curr.marked ? acc : acc + curr.number , 0)).reduce((acc, curr) => acc + curr, 0);
-
-    let [numbers, ...boards] = inputs.split(/\n\s*\n/);
-    numbers = numbers.split(",").map(Number);
-	const allBoards = arrangeDataAsBoardsMatrix(boards);
-    
+	const [ numbers, allBoards ] = getBoardsAndNumbersList(inputs);
+	
 	for (const num of numbers) {
 
 		for (const board of allBoards) {
@@ -51,5 +59,52 @@ const getBingoScore = (inputs) => {
 	}
 }
 
+const getLastWiningBoardScore = (inputs) => {
+	const [ numbers, allBoards ] = getBoardsAndNumbersList(inputs);
+	let winningNums = {
+		board: [],
+		num: 0,
+		biggestIndex: 0 
+	};
+	
+	for (const board of allBoards) {
+		let boardWon = false;
+		for (let i = 0; i < numbers.length; i++) {
+			
+			if (boardWon) {
+				break;
+			}
+
+			for (const line of board) {
+
+				const index = line.findIndex(({ number, marked }) => numbers[i] === number && !marked);
+				
+				if (index !== -1) {
+					line[index].marked = true;
+					
+					if (isWin(board, line, index)) {
+						boardWon = true;
+						
+						if (winningNums.biggestIndex < i) {
+							winningNums = { 
+								board, 
+								biggestIndex: i, 
+								num: numbers[i] 
+							};
+						}
+						
+						break;
+					}
+				}
+			}	
+		}
+	}
+
+	const { board, num } = winningNums;
+	const sum = sumAllUnmarked(board);
+	return sum * num;
+}
+
 
 console.log(`Bingo score: ${getBingoScore(inputs)}`);
+console.log(`Last winning bingo score: ${getLastWiningBoardScore(inputs)}`);
