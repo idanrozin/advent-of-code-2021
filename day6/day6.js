36af1df693766e5eeb5d10b370523f8af547e7ec
idanrozin <idanrozin@gmail.com>
2022-01-22 17:08:43 +0200

    finished day 6 question 2

--- day6/day6.js
+++ day6/day6.js
@@ -1,10 +1,10 @@
 /* https://adventofcode.com/2021/day/6 */
 
-const ReadFilesUtils = require('../utils/utils.js');
+const { ReadFilesUtils, Common } = require('../utils/utils.js');
 
 const inputs = new ReadFilesUtils(__dirname).rawInput.split(",").map(Number);
 
-const countFish = (inputs) => {
+const countLanternFish = (inputs) => {
     const countZeros = (numArr) => numArr.filter(n => n === 0).length;
     const decreaseNums = (numArr) => numArr.map(n => --n);
     const replaceZeros = (numArr) => numArr.map(n => n === -1 ? 6 : n);
@@ -18,5 +18,37 @@ const countFish = (inputs) => {
     return inputs;
 }
 
+const countLanternFishOptimized = (inputs) => {
 
-console.log(countFish(inputs).length);
+    //init & fill map
+    const counterArray = [...Array(9)].map( (_, i) => [i, 0]);
+    let fishMap = new Map([...counterArray]);
+    
+    inputs.forEach(num => {
+        fishMap.set(num, fishMap.get(num) + 1);
+    });
+    
+    for (let i = 0; i < 256; i++) {
+        const fishMapCopy = new Map(fishMap);
+        // count zeros
+        const zeros = fishMapCopy.get(0);
+        for (let i = 0; i < 9; i++) {
+            // decreaseNums
+            fishMap.set(i, fishMapCopy.get(i + 1));
+
+            if (i === 6) {
+                // 6's is comprised of the 7's from prev iteration + the zeros from last iteration which will be now 6 
+                fishMap.set(i, fishMapCopy.get(i + 1) + zeros);
+            }
+            if (i === 8) {
+                // 0 from last iteration becomes 8
+                fishMap.set(i, zeros);
+            }
+        }
+    }
+
+    return Common.sumReduce(Array.from(fishMap.values()));
+}
+
+console.log(`Lantern Fish count after 80 days: ${countLanternFish(inputs).length}`);
+console.log(`Lantern Fish count after 256 days (with optimization for space and time): ${countLanternFishOptimized(inputs)}`);
