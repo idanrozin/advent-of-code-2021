9689467318d17ebc633761c4149eae8fb0942e3d
idanrozin <idanrozin@gmail.com>
2022-01-29 16:36:51 +0200

    finished day7 question 2 and refactored function from q1 to be more generic as the base is the same

--- day7/day7.js
+++ day7/day7.js
@@ -4,12 +4,16 @@ const { ReadFilesUtils } = require('../utils/utils.js');
 
 const numbers = new ReadFilesUtils(__dirname).inputAsNumbers
 
-const calLeastFuelMoves = (numbers) => {
+const calLeastFuelMoves = (numbers,calcFunction) => {
     const numbersSet = [...new Set(numbers)];
-    const fuelSum = numbersSet.map(num => numbers.reduce((acc, curr) => acc + Math.abs(curr - num), 0));
+    const fuelSum = numbersSet.map(calcFunction);
     return Math.min(...fuelSum);
-}
-
-console.log(`Least Fuel Moves: ${calLeastFuelMoves(numbers)}`);
+};
 
 
+const countOneEnergyUnitPerStep = (num) => numbers.reduce((acc, curr) => acc + Math.abs(curr - num), 0);
+const countSteps = (n) => (n * (n + 1)) / 2;
+const countAscendingEnergyUnitPerStep = (num) => numbers.reduce((acc, curr) => acc + countSteps(Math.abs(curr - num)), 0);
+
+console.log(`Least Fuel Moves when each step is 1 energy unit: ${calLeastFuelMoves(numbers,countOneEnergyUnitPerStep )}`);
+console.log(`Least Fuel Moves when each step energy unit increased by 1 from previous step: ${calLeastFuelMoves(numbers, countAscendingEnergyUnitPerStep)}`);
