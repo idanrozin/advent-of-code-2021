b943c4165569e7fe869a697d86b009d66d38dd90
idanrozin <idanrozin@gmail.com>
2022-02-12 12:13:26 +0200

    added segments and starting strategy

--- day8/day8.js
+++ day8/day8.js
@@ -1,6 +1,18 @@
 /* https://adventofcode.com/2021/day/8 */
 
 const { ReadFilesUtils } = require('../utils/utils.js');
+const {
+    SEGMENT_0,
+    SEGMENT_1,
+    SEGMENT_2,
+    SEGMENT_3,
+    SEGMENT_4,
+    SEGMENT_5,
+    SEGMENT_6,
+    SEGMENT_7,
+    SEGMENT_8,
+    SEGMENT_9,
+} = require('./segments');
 
 const txt = new ReadFilesUtils(__dirname).inputAsStrings;
 const UNIQUE_VALS = [ 2, 4, 3, 7 ];
@@ -9,4 +21,38 @@ const getUniqueNums = (texts) => {
     const onlyUniqueNums = outputValues.filter(str => UNIQUE_VALS.includes(str.length));
     return onlyUniqueNums.length;
 }
-console.log(`number of unique numbers ${getUniqueNums(txt)}`);
+// console.log(`number of unique numbers ${getUniqueNums(txt)}`);
+/*
+ console.log(SEGMENT_0);
+ console.log(SEGMENT_1);
+ console.log(SEGMENT_2);
+ console.log(SEGMENT_3);
+ console.log(SEGMENT_4);
+ console.log(SEGMENT_5);
+ console.log(SEGMENT_6);
+ console.log(SEGMENT_7);
+ console.log(SEGMENT_8);
+ console.log(SEGMENT_9); 
+ */
+
+ const LETTERS = ['a', 'b', 'c', 'd', 'e', 'f', 'g'];
+ const ALL_SEGMENTS = [ SEGMENT_0, SEGMENT_1, SEGMENT_2, SEGMENT_3, SEGMENT_4, SEGMENT_5, SEGMENT_6, SEGMENT_7, SEGMENT_8, SEGMENT_9 ];
+//  console.log(ALL_SEGMENTS[0].indexOf('d'));
+ const deduceNumbers = (texts) => {
+    // count letter frequency in all digits
+    const counterArray = LETTERS.map( (l) => [l, 0]);
+    const letterScoreMap = new Map([...counterArray]);
+    const countLetterFrequencyInDigits = () => {
+        LETTERS.forEach(letter => {
+            ALL_SEGMENTS.forEach(segment => {
+                if (segment.indexOf(letter) !== -1) {
+                    letterScoreMap.set(letter, letterScoreMap.get(letter) + 1);
+                }
+            });
+        });
+    }
+    countLetterFrequencyInDigits();
+    console.log(letterScoreMap);
+ }
+ deduceNumbers()
+ 
---
+++ day8/segments.js
@@ -0,0 +1,102 @@
+const SEGMENT_0 = 
+`aaaa    
+b    c  
+b    c  
+ ....   
+e    f  
+e    f  
+ gggg `;
+ const SEGMENT_1 = 
+  `....    
+ .    c  
+ .    c  
+ ....   
+ .    f  
+ .    f  
+  ....`;
+
+  const SEGMENT_2 = 
+   `aaaa  
+  .    c
+  .    c
+   dddd
+  e    .
+  e    .
+   gggg`;
+
+   const SEGMENT_3 =
+ `aaaa 
+ .    c
+ .    c
+  dddd 
+ .    f
+ .    f
+  gggg `;
+
+const SEGMENT_4 = 
+`....
+b    c
+b    c
+ dddd
+.    f
+.    f
+ ....`;
+
+ const SEGMENT_5 = 
+`aaaa 
+b    .
+b    .
+ dddd 
+.    f
+.    f
+ gggg`;
+
+const SEGMENT_6 = 
+`aaaa  
+b    .
+b    .
+ dddd 
+e    f
+e    f
+ gggg `;
+
+
+ const SEGMENT_7 =
+`aaaa 
+.    c
+.    c
+ .... 
+.    f
+.    f
+ ....`;
+
+ const SEGMENT_8 =
+`aaaa 
+b    c
+b    c
+ dddd 
+e    f
+e    f
+ gggg`;
+
+const SEGMENT_9 = 
+`aaaa
+b    c
+b    c
+ dddd
+.    f
+.    f
+ gggg`;
+
+module.exports = {
+    SEGMENT_0,
+    SEGMENT_1,
+    SEGMENT_2,
+    SEGMENT_3,
+    SEGMENT_4,
+    SEGMENT_5,
+    SEGMENT_6,
+    SEGMENT_7,
+    SEGMENT_8,
+    SEGMENT_9,
+};
