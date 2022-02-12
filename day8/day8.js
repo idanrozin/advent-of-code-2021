bea1381be05c4ec1904577a937377481d7fcaf81
idanrozin@gmail.com <idanrozin@gmail.com>
2022-02-12 21:27:36 +0200

    finished day8 q2 (hard question!!!)

--- day8/day8.js
+++ day8/day8.js
@@ -1,6 +1,6 @@
 /* https://adventofcode.com/2021/day/8 */
 
-const { ReadFilesUtils } = require('../utils/utils.js');
+const { ReadFilesUtils, Common } = require('../utils/utils.js');
 const {
     SEGMENT_0,
     SEGMENT_1,
@@ -14,45 +14,65 @@ const {
     SEGMENT_9,
 } = require('./segments');
 
-const txt = new ReadFilesUtils(__dirname).inputAsStrings;
+const inputs = new ReadFilesUtils(__dirname).inputAsStrings;
 const UNIQUE_VALS = [ 2, 4, 3, 7 ];
 const getUniqueNums = (texts) => {
     const outputValues = [].concat(...texts.map(str => str.split("|")[1].trim().split(" ")));
     const onlyUniqueNums = outputValues.filter(str => UNIQUE_VALS.includes(str.length));
     return onlyUniqueNums.length;
 }
-// console.log(`number of unique numbers ${getUniqueNums(txt)}`);
-/*
- console.log(SEGMENT_0);
- console.log(SEGMENT_1);
- console.log(SEGMENT_2);
- console.log(SEGMENT_3);
- console.log(SEGMENT_4);
- console.log(SEGMENT_5);
- console.log(SEGMENT_6);
- console.log(SEGMENT_7);
- console.log(SEGMENT_8);
- console.log(SEGMENT_9); 
- */
+
 
  const LETTERS = ['a', 'b', 'c', 'd', 'e', 'f', 'g'];
  const ALL_SEGMENTS = [ SEGMENT_0, SEGMENT_1, SEGMENT_2, SEGMENT_3, SEGMENT_4, SEGMENT_5, SEGMENT_6, SEGMENT_7, SEGMENT_8, SEGMENT_9 ];
-//  console.log(ALL_SEGMENTS[0].indexOf('d'));
+
  const deduceNumbers = (texts) => {
-    // count letter frequency in all digits
-    const counterArray = LETTERS.map( (l) => [l, 0]);
-    const letterScoreMap = new Map([...counterArray]);
-    const countLetterFrequencyInDigits = () => {
+    // count all instances in array that the letter is used
+    const countLetterFrequencyInDigits = (segments) => {
+        // init score map
+        const counterArray = LETTERS.map(l => [l, 0]);
+        const letterScoreMap = new Map([...counterArray]);
+        
         LETTERS.forEach(letter => {
-            ALL_SEGMENTS.forEach(segment => {
+            segments.forEach(segment => {
                 if (segment.indexOf(letter) !== -1) {
                     letterScoreMap.set(letter, letterScoreMap.get(letter) + 1);
                 }
             });
         });
-    }
-    countLetterFrequencyInDigits();
-    console.log(letterScoreMap);
+
+        return letterScoreMap;
+    };
+
+    // calculate sum according to the occurnces in segments
+    const countSumByLetterFrequency = (letterFrequencyMap, _segments) => {
+
+        const initDigitsArray = [...Array(_segments.length)].map( (_, i) => [i, 0]);
+        const digitsScores = new Map([...initDigitsArray]);
+
+        _segments.forEach((seg,i) => {
+            LETTERS.forEach(letter => {
+                if (seg.indexOf(letter) !== -1) {
+                    digitsScores.set(i, digitsScores.get(i) + letterFrequencyMap.get(letter));
+                }
+            });
+        });
+
+        return digitsScores;
+    };
+    
+    const allDigitsScore = countSumByLetterFrequency(countLetterFrequencyInDigits(ALL_SEGMENTS), ALL_SEGMENTS);
+    const scoreToDigitMap = new Map(Array.from(allDigitsScore, a => a.reverse()));
+    
+    const decipheredNums = texts.map((input) => {
+        const [digits, output] = input.split("|");
+    
+        const digitScore = countSumByLetterFrequency(countLetterFrequencyInDigits(digits.split(" ")), output.split(" "));
+        return Number(Array.from(digitScore.values()).map(num => scoreToDigitMap.get(num)).join(""));
+    });
+    
+    return Common.sumReduce(decipheredNums);
  }
- deduceNumbers()
+ console.log(`number of unique numbers ${getUniqueNums(inputs)}`);
+ console.log(`sum of all deciphered numbers ${deduceNumbers(inputs)}`);
  
